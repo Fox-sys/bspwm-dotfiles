fc42fd6a9ff2b651d3177609528978dad53d7304
Your Name <you@example.com>
2024-04-23 07:21:28 +0300

    Пофиксить установку драйверов

--- Builder/src/adapters/system_manipulation/installers/driver_installers/base_driver_installer.py
+++ Builder/src/adapters/system_manipulation/installers/driver_installers/base_driver_installer.py
@@ -1,3 +1,4 @@
+import os
 from abc import abstractmethod, ABC
 
 from src.adapters.system_manipulation.installers.base_installer import BaseInstaller
@@ -5,4 +6,16 @@ from src.application.configurator.interfaces.system_manipulators import IDriverI
 
 
 class BaseDriverInstaller(BaseInstaller, IDriverInstaller, ABC):
-    ...
+    def _prepare_multilib(self):
+        os.system(r"sudo sed -i 's/^#\[multilib\]/[multilib]/' /etc/pacman.conf")
+        os.system(
+            r"sudo sed -i '/^\[multilib\]$/,/^\[/ s/^#\(Include = \/etc\/pacman\.d\/mirrorlist\)/\1/' /etc/pacman.conf")
+
+    def _install_multilib(self):
+        os.system("sudo pacman -Sl multilib")
+        os.system("sudo pacman -Sy")
+
+    def install_packages(self) -> None:
+        self._prepare_multilib()
+        self._install_multilib()
+        super(BaseInstaller).install_packages()
